e41bdb5890c4d690c01840795a237254c51f6927
Admin <admin@home.ru>
2021-08-21 11:25:39 +0400

    Remake silver and bronze medals

--- resources/views/main_table.blade.php
+++ resources/views/main_table.blade.php
@@ -27,42 +27,58 @@
 			}
 			
 			.self {
-				min-width: 39px;
-				max-width: 39px;
+				min-width: 2.5rem;
+				max-width: 2.5rem;
+				min-height: 2.5rem;
+				max-height: 2.5rem;
 				padding: 0;				
-				position: relative;
+				/*position: relative;*/
 				
 				
 				background: linear-gradient(170deg, rgb(245,245,245) 0%,
 													rgb(225,235,235) 50%,
 													rgb(175,195,220) 100%);
 			}
-			
+			/*
 			.medal {
 				width: 100%;
 				vertical-align: middle;
-			}
+			}*/
 			
-			.med {
+			.medal {
 				width: 2rem;
 				height: 2rem;
-				vertical-align: middle;
 				border-radius: 1rem;
-				box-shadow: 1px 1px 1px rgb(255,180,68);
 				margin: auto auto;
-				background: linear-gradient(170deg, rgb(252, 220, 107) 0%,
-													
-													rgb( 255, 254, 182) 100%);
-													
-				font-family: 'GFS Didot', serif;
-				/*color: rgb(234,158,46);*/
-				color: rgb(255, 254, 182);
+				vertical-align: middle;													
+				font-family: 'GFS Didot', serif;				
 				display: flex;
 				align-items: center;
-				justify-content: center;
+				justify-content: center;				
+			}
+			
+			.gold {
+				background: linear-gradient(170deg, rgb(252, 220, 107) 0%,													
+													rgb(255, 254, 182) 100%);
+				box-shadow: 1px 1px 1px rgb(255,180,68);
+				color: rgb(255, 254, 182);
 				text-shadow: 1px 1px 1px rgb(255,180,68);
-				/*border: 1px dotted rgb(234,158,46);*/
-				
+			}
+			
+			.silver {
+				background: linear-gradient(170deg, rgb(220,220,220) 0%,													
+													rgb(250,240,230) 100%);
+				box-shadow: 1px 1px 1px rgb(220,220,220);
+				color: rgb(250,240,230);
+				text-shadow: 1px 1px 1px rgb(220,220,220);
+			}
+			
+			.bronze {
+				background: linear-gradient(170deg, rgb(237,173,102) 0%,													
+													rgb(255,219,44) 100%);
+				box-shadow: 1px 1px 1px rgb(237,173,102);
+				color: rgb(255,219,44);
+				text-shadow: 1px 1px 1px rgb(237,173,102);
 			}
 			
 			.totalScore {
@@ -117,16 +133,18 @@
 									switch($loop->iteration) {
 										case 1:
 										 //echo "<img src='pics/chess_gold_t.png' class='medal'>";
-										 echo '<div class="med">I</>';
+										 echo '<div class="medal gold">I</>';
 										 
 										 break;
 										
 										case 2:
-										 echo "<img src='pics/chess_silver_t.png' class='medal'>";
+										 //echo "<img src='pics/chess_silver_t.png' class='medal'>";
+										 echo '<div class="medal silver">II</>';
 										 break;
 										 
 										case 3:
-										 echo "<img src='pics/bronze_3_t.png' class='medal'>";
+										 //echo "<img src='pics/bronze_3_t.png' class='medal'>";
+										 echo '<div class="medal bronze">III</>';
 										 break;										 
 									}
 									@endphp
