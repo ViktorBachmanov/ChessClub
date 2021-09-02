13e72dc5d60f9d40ea9af3b36ea48a0121563e39
Admin <admin@home.ru>
2021-09-02 11:08:39 +0400

    Correct

--- resources/views/components/eval-rating.blade.php
+++ resources/views/components/eval-rating.blade.php
@@ -1,21 +1,20 @@
 <section style='white-space: pre'>
-	public function evalRating($opponentRating, $score)
+    public function evalRating($opponentRating, $score)
     {
-		$rating = $this->rating;
+        $rating = $this->rating;
         $expectedScore = 1 / (1 + pow(10, ($opponentRating - $rating) / 400));
 		
+        $gamesTotal = $this->getTotalGames();
+        $koef;
+        if($rating >= 2400)
+            $koef = 10;
+        else if($gamesTotal > 30)
+            $koef = 20;
+        else
+            $koef = 40;
 		
-		$gamesTotal = $this->getTotalGames();
-		$koef;
-		if($rating >= 2400)
-			$koef = 10;
-		else if($gamesTotal > 30)
-			$koef = 20;
-		else
-			$koef = 40;
+        $this->rating = $rating + $koef * ($score - $expectedScore);
 		
-		$this->rating = $rating + $koef * ($score - $expectedScore);
-		
-		$this->save();
+        $this->save();
     }
 </section>
