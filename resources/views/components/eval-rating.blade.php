0b93887d1acf0d55dd589ac90bec701e035e4456
Admin <admin@home.ru>
2021-09-09 13:36:40 +0400

    Decrease tab in code snippet

--- resources/views/components/eval-rating.blade.php
+++ resources/views/components/eval-rating.blade.php
@@ -1,20 +1,20 @@
 <code class='language-php' style=' '>
-    public function evalRating($opponentRating, $score)
-    {
-        $rating = $this->rating;
-        $expectedScore = 1 / (1 + pow(10, ($opponentRating - $rating) / 400));
-		
-        $koef;
-        if($rating >= 2400)
-            $koef = 10;
-        else if($this->getTotalGames() > 30)
-            $koef = 20;
-        else
-            $koef = 40;
-		
-        $this->rating = $rating + $koef * ($score - $expectedScore);
-		
-        $this->save();
-    }
+public function evalRating($opponentRating, $score)
+{
+	$rating = $this->rating;
+	$expectedScore = 1 / (1 + pow(10, ($opponentRating - $rating) / 400));
+	
+	$koef;
+	if($rating >= 2400)
+		$koef = 10;
+	else if($this->getTotalGames() > 30)
+		$koef = 20;
+	else
+		$koef = 40;
+	
+	$this->rating = $rating + $koef * ($score - $expectedScore);
+	
+	$this->save();
+}
 	
 </code>
