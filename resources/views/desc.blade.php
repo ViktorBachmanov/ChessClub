de72a521d7ac118e766451e3a0b7654667f1525d
Admin <admin@home.ru>
2021-09-26 17:58:32 +0400

    Correct desc page

--- resources/views/desc.blade.php
+++ resources/views/desc.blade.php
@@ -11,7 +11,6 @@
 
 		<link href="https://fonts.googleapis.com/css2?family=GFS+Didot&display=swap" rel="stylesheet">
 	
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	
 		<link rel="Stylesheet" href="css/base.css?13">
 		<link rel="Stylesheet" href="css/main_table.css?1">
@@ -238,6 +237,6 @@
 		
     </body>
 
-	<script src='js/SmartTableBabel.js?7'></script>
+	<script src='js/SmartTable.js?7'></script>
 	
 </html>
